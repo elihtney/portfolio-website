e99f00bf8382c351559d71ee1679d79074e08d3e
elihtney <ytl1321@jagmail.southalabama.edu>
2020-09-04 02:33:00 -0600

    Summary
    - Changes made to fix Contact option on larger screens (no more hamburger menu)
    - Fixed CV Download button
    - Fixed button hyperlink color

--- js/script.js
+++ js/script.js
@@ -2,9 +2,14 @@ console.log('Hello there! Welcome to my website!')
 
 function menuToggle () {
   var x = document.getElementById('myNavtoggle')
-  if (x.className === 'navtoggle') {
+  if (x.className === 'navtoggle' && (getViewportWidth() < 640)) {
     x.className += ' responsive'
   } else {
     x.className = 'navtoggle'
   }
+}
+
+function getViewportWidth () {
+  console.log(window.innerWidth)
+  return (window.innerWidth)
 }
